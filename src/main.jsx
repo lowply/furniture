9eb42b6bc7bf71b369f6972dfcfaf976917b0e82
Sho Mizutani <lowply@github.com>
2017-01-01 21:35:38 +0900

    Added http URL bookmarks in the share button part

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 // React 15.0.1
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 // Getting error "jQuery is not defined" when I put jQuery and Bootstrap at the same time even if I did like this:
 // import jQuery from 'jquery';
 // const $ = jQuery;
 // Since I don't really use jQuery and Bootstrap's JavaScript plugins, leaving it commented out for now.
 // More info: https://github.com/twbs/bootstrap/issues/17201
 //
-// import $ from 'jquery';
+import $ from 'jquery';
 // import Bootstrap from 'bootstrap';
 import Highlight from 'highlight.js';
 
 Highlight.initHighlightingOnLoad();
+
+var image_url = $(".hb-button.http a").attr("href").replace(/entry\/http/, 'entry\/image\/http')
+$(".hb-button.http a").append('<img src="' + image_url + '" />')
